41394b34d648613bb3562303b02e74fac80f91b8
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-08-30 18:57:46 +0900

    ユーザー登録画面エラーメッセージ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #validates :nickname, presence: true
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :password, confirmation: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "Password Include both letters and numbers" 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  validates :birth_date, presence: true
 end
