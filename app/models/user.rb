761f59c6050447201b607bc5443ed701ae52718f
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-09-04 12:36:15 +0900

    単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: {minimum: 6}
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "Password Include both letters and numbers" 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :family_name
     validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン一]+\z/, message: 'Full-width katakana characters' } do
     validates :family_name_kana
     validates :first_name_kana
   end
   validates :birth_date, presence: true
 end
+ 
+    
