75d594f33c88d6661124b1fd5dff13c2c3f48630
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-09-06 21:18:50 +0900

    ActiveHashのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   #has_one :purchase  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :origin_area
+  belongs_to_active_hash :shipment_date
+  validates :category_id, numericality: { other_than: 1 }
 end
