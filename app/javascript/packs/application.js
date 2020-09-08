ca14a86a728fc22f4b11205367be08bbb911665d
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-09-08 16:39:56 +0900

    javascript実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../packs/calc")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//# sourceMappingURL=application-708efcef47ddbdedb7b4.js.map
