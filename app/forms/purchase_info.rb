55d4a46bc114b4d4dc7fb5ab9b3425e5ceb52c67
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-09-15 21:07:54 +0900

    微調整とバリデーションの設定

--- app/forms/purchase_info.rb
+++ app/forms/purchase_info.rb
@@ -5,11 +5,28 @@ class PurchaseInfo
 
  # 切り取ったバリデーションをここに移動
   with_options presence: true do
+    validates :postal_code        # 郵便番号
+    validates :prefectures        # 都道府県
+    validates :municipalities     # 市区町村
+    validates :address            # 番地
+    validates :phone_number       # 電話番号
+    validates :token              # トークン
   end
+
+  # 都道府県のバリデーション
+  validates :prefectures, numericality: { other_than: 1 , message: 'Select'}
+
+  # 郵便番号のバリデーション
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  validates_format_of :postal_code, with: POSTAL_CODE_REGEX, message: "need hyphen" 
+  
+  # 電話番号のバリデーション
+  PHONE_NUMBER_REGEX = /\A[0-9]+\z/
+  validates_format_of :phone_number, with: PHONE_NUMBER_REGEX, message: "must be half-width numbers" 
+  validates :phone_number, length: {maximum: 11}
 
 
   def save
-    binding.pry
     begin
       # 配送先の情報を保存
       purchase = Purchase.create(user_id: user_id, item_id: item_id)
