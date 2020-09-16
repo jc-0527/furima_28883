b7d1042b48e7dc8edc73779d053f492faf3ca5ef
jc-0527 <67852985+jc-0527@users.noreply.github.com>
2020-09-16 17:17:18 +0900

    単体テストコード実装

--- spec/factories/purchase_infos.rb
+++ spec/factories/purchase_infos.rb
@@ -3,13 +3,22 @@ FactoryBot.define do
     association :user
     association :item
 
-    # postal_code          {Faker::Number.between(2, 7)}
+    postal_code          {"333-4444"} # {Faker::Number.between(2, 7)}
     prefectures          {Faker::Number.between(2, 48)}
     municipalities       {Faker::String.random(length: 50)}
-    address              {{Faker::String.random(length: 100)}}
+    address              {Faker::String.random(length: 100)}
     building_name        {Faker::String.random(length: 100)}
     phone_number         {Faker::Number.number(digits: 11)}
+    token                {Faker::String.random(length: 100)}
     # purchase_id          {Faker::Number.between(2, 4)}
-    
+
+    # postal_code          {"333-4444"}
+    # prefectures          {2}
+    # municipalities       {"大阪市天王寺区"}
+    # address              {"3-9-11"}
+    # building_name        {"大阪ビル"}
+    # phone_number         {"12345678333"}
+    # token                {"aaaaaa"}
+    # # purchase_id          {Faker::Number.between(2, 4)}
   end
 end
--- spec/models/purchase_info_spec.rb
+++ spec/models/purchase_info_spec.rb
@@ -2,113 +2,74 @@ require 'rails_helper'
 
 RSpec.describe PurchaseInfo, type: :model do
   before do
-    @user = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item)
-    @ = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item)
+    user = FactoryBot.build(:user)
+    item = FactoryBot.build(:item)
+    @purchase_info = FactoryBot.build(:purchase_info, user_id: user.id, item_id: item.id )
   end
 
   describe '購入機能' do
     context '購入がうまくいくとき' do
-      it "nicknameとemail、passwordとpassword_confirmation,family_name,first_nameとfamily_name_kana,first_name_kanaとbirth_dateが存在すれば登録できる" do
-        expect(@user).to be_valid
+      it "postal_codeとprefectures、municiplitiesとaddress,building_name,phone_numberとpurchase_id,user_idとitem_idが存在すれば登録できる" do
+        expect(@purchase_info).to be_valid
       end
     end
 
-    context '新規登録がうまくいかないとき' do
-      it "nicknameが空だと登録できない" do
-        @user.nickname = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    context '購入がうまくいかないとき' do
+      it "postal_codeが空だと登録できない" do
+        @purchase_info.postal_code = ''
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Postal code can't be blank")
       end
-      it "emailが空では登録できない" do
-        @user.email = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+      it "municipalitiesが一意でなかったら登録できない" do
+        @purchase_info.municipalities = ''
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Municipalities can't be blank")
       end
-      it "emailが一意でなかったら登録できない" do
-        @user.save
-        another_user = FactoryBot.build(:user)
-        another_user.email = @user.email
-        another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      it "addressが空では登録できない" do
+        @purchase_info.address = ''
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Address can't be blank")
       end
-      it "emailに＠が含まれていなければ、登録できない" do
-        @user.email = 'abc.com'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+      it "phone_numberが空では登録できない" do
+        @purchase_info.phone_number = ''
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Phone number can't be blank")
       end
-      it "passwordが空では登録できない" do
-        @user.password = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+      it "tokenが空では登録できない" do
+        @purchase_info.token = ''
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Token can't be blank")
       end
-      it "passwordが6文字未満であれば登録できない" do
-        @user.password = 'a1!Kz'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      it "prefecturesが1(--)では登録できない" do
+        @purchase_info.prefectures = 1
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Prefectures Select")
       end
-      it "パスワードは半角英数字混合でなければ登録できない(※英字のみで検証)" do
-        @user.password = 'aaaa'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password Password Include both letters and numbers")
+      it "postal_codeは○○○-○○○○の形式でなければ登録できない" do
+        @purchase_info.postal_code = "1111111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Postal code Input correctly")
+        @purchase_info.postal_code = "11111111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Postal code Input correctly")
+        @purchase_info.postal_code = "1111-1111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Postal code Input correctly")
+        @purchase_info.postal_code = "111-11111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Postal code Input correctly")
       end
-      it "パスワードは半角英数字混合でなければ登録できない(※数字のみで検証)" do
-        @user.password = '1111'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password Password Include both letters and numbers")
+      it "phone_numberは11桁の半角数字の形式でなければ登録できない" do
+        @purchase_info.phone_number = "1111-111111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Phone number must be half-width numbers")
+        @purchase_info.postal_code = "11111-111111"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Phone number must be half-width numbers")
+        @purchase_info.postal_code = "aaaaaaaaaa1"
+        @purchase_info.valid?
+        expect(@purchase_info.errors.full_messages).to include("Phone number must be half-width numbers")
       end
-      it "passwordが存在してもpassword_confirmationが空では登録できない" do
-        @user.password_confirmation = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-      end
-      it "family_nameが空では登録できない" do
-        @user.family_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Family name can't be blank")
-      end
-      it "family_nameが半角だと登録できない" do
-        @user.family_name = 'ｱｲｳｴｵ'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Family name Full-width characters")
-      end
-      it "first_nameが空では登録できない" do
-        @user.first_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank")
-      end
-      it "first_nameが半角だと登録できない" do
-        @user.first_name = 'ｶｷｸｹｺ'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name Full-width characters")
-      end
-      it "family_name_kanaが空では登録できない" do
-        @user.family_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
-      end
-      it "family_name_kanaが全角カタカナ以外だと登録できない" do
-        @user.family_name_kana = 'あア亜a'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana Full-width katakana characters")
-      end
-      it "first_name_kanaが空では登録できない" do
-        @user.first_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank")
-      end
-      it "first_name_kanaが全角カタカナ以外だと登録できない" do
-        @user.first_name_kana = 'いイ伊i'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana Full-width katakana characters")
-      end
-      it "birth_dateが空では登録できない" do
-        @user.birth_date = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Birth date can't be blank")
-      end
-
     end
   end
 end
